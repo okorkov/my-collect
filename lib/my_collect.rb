428351573e7d927c258e28ade4574d8b252ad994
okorkov <alex.okorkov@gmail.com>
2020-11-10 14:57:27 -0800

    commit tree 034a8b6a828556c87341273a9e12ad87791da15b

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
 
 
+def my_collect(array)
+    counter = 0
+    result = Array.new
+    while counter < array.length
+        result.push(yield array[counter])
+        counter +=1
+    end
+    result
+end
